da1d35ec85e47b72f8b5751765b4a4fb19cd4308
Ilya Korobka <ilkrbk@MacBook-Pro.local>
2021-07-17 16:39:51 +0300

    add request

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
 	const [dataObj, setDataObj] = useState()
 	const [loader, setLoader] = useState(true)
 	const [activeTab, setActiveTab] = useState(0)
+	const [stageId, setStageId] = useState()
+	const [defaultVar] = useState({
+		tournamentId: 46,
+		seasonId: 15429
+	})
 
 	const tabs = [
 		{
@@ -25,27 +30,33 @@ function App() {
 		}
 	]
 
-	const tabClick = async (type) => {
+	const tabClick = async (type, stageId) => {
 		setLoader(true)
-		await axios.get(`https://ss2.tjekscores.dk/tournaments/46/standings?&addResults=true&resultsLimit=6&type=${type}&form=last&stageId=868958`)
-        .then(response => {
-			setDataObj(response.data.sort((a, b) => a.points > b.points ? -1 : 1))
-		});
-		setActiveTab(type)
+		await axios.get(`https://ss2.tjekscores.dk/tournaments/${defaultVar.tournamentId}/standings?&addResults=true&resultsLimit=6&type=${type}&form=last&stageId=${stageId}`)
+        .then(response => setDataObj(response.data.sort((a, b) => a.points > b.points ? -1 : 1)));
 		await setTimeout(() => {
+			setActiveTab(type)
 			setLoader(false)
 		}, 200);
 	}
 
+	const requestStageId = async () => {
+		await axios.get(`https://ss2.tjekscores.dk/tournaments/${defaultVar.tournamentId}/season?seasonId=${defaultVar.seasonId}`)
+		.then(response => {
+			setStageId(response.data.season.stages[0].id)
+			tabClick('', response.data.season.stages[0].id)
+		});
+	}
+
 	useEffect(() => {
-		tabClick('')
+		requestStageId()
 	}, []);
 
 	return (
 		<div className="App">
 			<ul className="tab__list">
 				{tabs.map((tab) => (
-					<Tab click={tabClick} activeTab={activeTab} type={tab.type} key={tab.id} text={tab.text}/>
+					<Tab click={tabClick} stageId={stageId} activeTab={activeTab} type={tab.type} key={tab.id} text={tab.text}/>
 				))}
 			</ul>
 			{loader ?
--- src/components/tab/tab.js
+++ src/components/tab/tab.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const Tab = ({text, click, type, activeTab}) => {
+export const Tab = ({text, click, type, activeTab, stageId}) => {
     if (activeTab === type) {
-        return <li onClick={() => (click(type))} className="tab__item tab__item-active">{text}</li>
+        return <li onClick={() => (click(type, stageId))} className="tab__item tab__item-active">{text}</li>
     }
-    return <li onClick={() => (click(type))} className="tab__item">{text}</li>
+    return <li onClick={() => (click(type, stageId))} className="tab__item">{text}</li>
 }
