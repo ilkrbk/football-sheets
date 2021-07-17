1e83d6e931da6162b37e5d7aae7c3224aa059739
Ilya Korobka <ilkrbk@MacBook-Pro.local>
2021-07-17 15:38:11 +0300

    make logic sheet

--- src/components/sheet/sheet-item/sheet-item.js
+++ src/components/sheet/sheet-item/sheet-item.js
@@ -1,30 +1,35 @@
 import React from 'react';
+import { HistoryGameItem } from './history-game-item/history-game-item';
 import "./main.sass";
 
-export const SheetItem = () => {
+export const SheetItem = ({item}) => {
+    const teamName = () => {
+        if (item.teamId === item.results[0].homeId) {
+            return item.results[0].homeName
+        } else {
+            return item.results[0].awayName
+        }
+    }
     return(
         <li className="sheet__item">
             <div className="sheet__item-main">
-                <span className="sheet__id">1</span>
-                <img src="http://pngimg.com/uploads/fcb_logo/fcb_logo_PNG24.png" alt="logo" className="sheet__img"/>
-                <h3 className="sheet__name">Barselona</h3>
+                <span className="sheet__id">{item.rank}</span>
+                <img src={`https://dxugi372p6nmc.cloudfront.net/spdk/current/64x64/${item.teamId}/teamlogo.png`} alt="logo" className="sheet__img"/>
+                <h3 className="sheet__name">{teamName()}</h3>
             </div>
             <div className="sheet__item-info">
                 <ul className="sheet__list-text-info">
-                    <li className="sheet__item-text-info">12</li>
-                    <li className="sheet__item-text-info">12</li>
-                    <li className="sheet__item-text-info">12</li>
-                    <li className="sheet__item-text-info">12</li>
-                    <li className="sheet__item-text-info">31-12</li>
-                    <li className="sheet__item-text-info">12</li>
+                    <li className="sheet__item-text-info">{item.matchesPlayed}</li>
+                    <li className="sheet__item-text-info">{item.matchesWon}</li>
+                    <li className="sheet__item-text-info">{item.matchesDraw}</li>
+                    <li className="sheet__item-text-info">{item.matchesLost}</li>
+                    <li className="sheet__item-text-info">{item.goalsScored}-{item.goalsConceded}</li>
+                    <li className="sheet__item-text-info">{item.points}</li>
                 </ul>
-                <ul className="steet__list-history-game">
-                    <li className="steet__item-history-game"></li>
-                    <li className="steet__item-history-game"></li>
-                    <li className="steet__item-history-game"></li>
-                    <li className="steet__item-history-game"></li>
-                    <li className="steet__item-history-game"></li>
-                    <li className="steet__item-history-game"></li>
+                <ul className="sheet__list-history-game">
+                    {item.results.map((circle) => (
+                        <HistoryGameItem key={circle.eventId} color={circle.outcome}/> 
+                    ))}
                 </ul>
             </div>
         </li>
--- src/components/sheet/sheet.js
+++ src/components/sheet/sheet.js
@@ -2,21 +2,14 @@ import React from 'react';
 import { SheetHeader } from './sheet-header/sheet-header';
 import { SheetItem } from "./sheet-item/sheet-item";
 
-
-export const Sheet = () => {
+export const Sheet = ({data}) => {
     return(
         <section className="sheet">
             <SheetHeader />
             <ul className="sheet__list">
-                <SheetItem/>
-                <SheetItem/>
-                <SheetItem/>
-                <SheetItem/>
-                <SheetItem/>
-                <SheetItem/>
-                <SheetItem/>
-                <SheetItem/>
-                <SheetItem/>
+                {data.map((item) => (
+                    <SheetItem key={item.rank} item={item}/>
+                ))}
             </ul>
         </section>
     )
