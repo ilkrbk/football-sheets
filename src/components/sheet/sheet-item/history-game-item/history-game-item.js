a0b7b39accfd63cb9fb5d030df80ffb556d6e4b8
Ilya Korobka <ilkrbk@MacBook-Pro.local>
2021-07-18 14:45:50 +0300

    change if on switch

--- src/components/sheet/sheet-item/history-game-item/history-game-item.js
+++ src/components/sheet/sheet-item/history-game-item/history-game-item.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 export const HistoryGameItem = ({color}) => {
-    if (color === 'win') {
-        return <li className="sheet__item-history-game green"></li>
-    } else if (color === 'loss') {
-        return <li className="sheet__item-history-game red"></li>
-    } else {
-        return <li className="sheet__item-history-game"></li>
+    switch (color) {
+        case 'win': return <li className="sheet__item-history-game green"></li>;
+        case 'loss': return <li className="sheet__item-history-game red"></li>
+        default: return <li className="sheet__item-history-game"></li>
     }
 }
